6ded1e73f01774b6af0aec45b2626b27fcc777ed
ElinorSel <Elinor.selinder@gmail.com>
2026-04-08 16:51:32 +0200

    test

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,14 @@ public class CameraFollow : MonoBehaviour
 
     private Vector3 velocity = Vector3.zero;
 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        GetComponent<Camera>().aspect = (Screen.width * 0.5f) / Screen.height;
+    }
     private void LateUpdate()
     {
         if (target == null) return;
